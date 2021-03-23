b296d047646a83c2336fc8ee95d1e79ef0d20534
leolpaz <leonardolpaz95@gmail.com>
2021-03-23 16:56:12 -0300

    Stablish the flow with the game, not accepting wrong

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
 class Board
-  attr_accessor :game
+  attr_accessor :game, :valid
+
   def initialize
     @square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @game = true
+    @valid = false
   end
 
   def create_board
@@ -19,45 +21,41 @@ class Board
     @input = input
     if !@square.include?(input)
       system 'cls'
-      puts "Please use a valid number"
-      sleep 2
+      puts 'Please use a valid number'
+      sleep 1
+      @valid = false
+    else
+      @valid = true
     end
   end
 
   def check_win(input)
-    case 
-    when input == 9
+    case input
+    when 9
       @game = false
-      system "clear"
-      system "cls"
+      system 'clear'
+      system 'cls'
       puts "Congratulations, you're the winner"
       sleep 1
-    when input == 8
+    when 8
       @game = false
-      system "clear"
-      system "cls"
-      puts "Tie!"
+      system 'clear'
+      system 'cls'
+      puts 'Tie!'
       sleep 1
     end
   end
 end
 
-class Player
-  def initialize(name)
-    @name = name.to_s
-  end
-end
-
-player1 = Player.new(@name)
 puts 'enter player one name?'
-player1 = gets.chomp
-
-player2 = Player.new(@name)
-
+player1 = gets.chomp.to_s
 puts 'enter player two name?'
-player2 = gets.chomp
+player2 = gets.chomp.to_s
 
 board = Board.new
+
+system 'clear'
+system 'cls'
 
 puts board.create_board
 
@@ -65,29 +63,32 @@ puts "#{player1} will be X"
 
 puts "#{player2} will be O"
 
-sleep 2
-
-
+sleep 1
 
 while board.game
-  system "clear"
+  system 'clear'
   system 'cls'
-  puts board.create_board
-  puts "It's #{player1} turn"
-  puts ''
-  puts ' Select an available cell'
-  input = gets.chomp.to_i
-  board.check_valid_move(input)
+  until board.valid
+    puts board.create_board
+    puts "It's #{player1} turn"
+    puts ''
+    input = gets.chomp.to_i
+    board.check_valid_move(input)
+  end
   board.check_win(input)
-  system "clear"
+  system 'clear'
   system 'cls'
-  puts board.create_board
-  puts "It's #{player2} turn"
-  puts ''
-  input = gets.chomp.to_i
-  board.check_valid_move(input)
+  board.valid = false
+  until board.valid
+    puts board.create_board
+    puts "It's #{player2} turn"
+    puts ''
+    input = gets.chomp.to_i
+    board.check_valid_move(input)
+  end
   board.check_win(input)
-  system "clear"
+  board.valid = false
+  system 'clear'
   system 'cls'
   puts board.create_board
 end
