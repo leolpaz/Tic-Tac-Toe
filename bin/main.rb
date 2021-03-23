9277f0d031df986093df8cb3214989c90255defd
leolpaz <leonardolpaz95@gmail.com>
2021-03-23 11:56:42 -0300

    Create board

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,22 @@
 #!/usr/bin/env ruby
-puts 'Hello World'
+
+class Board
+  def initialize
+    @square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  def create_board 
+    puts "-----+-----+-----"
+    puts "[ #{@square[0]} ] [ #{@square[1]} ] [ #{@square[2]} ]"
+    puts "[ #{@square[3]} ] [ #{@square[4]} ] [ #{@square[5]} ]"
+    puts "[ #{@square[6]} ] [ #{@square[7]} ] [ #{@square[8]} ]"
+    puts "-----+-----+-----"
+  end
+end
+
+
+
+
+
+board = Board.new
+
+puts board.create_board
