052d802b7b855da2cdfe63c5b2bf43a4e114d66f
leolpaz <leonardolpaz95@gmail.com>
2021-03-29 12:50:29 -0300

    Create testing case for the method #check_valid_move

---
+++ spec/logic_testing.rb
@@ -0,0 +1,18 @@
+require_relative "../lib/game_logic.rb"
+
+RSpec.describe Board do
+  let (:board) {Board.new}
+
+  describe '#check_valid_move' do
+
+    it 'returns true if the input is on the square array' do
+    expect(board.check_valid_move(1)).to be true
+    end
+
+    it 'returns false if the input is not on the array' do
+    expect(board.check_valid_move(67)).to be false
+    end
+
+  end
+  
+end
